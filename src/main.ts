b3b46ce2db7f42f031b0a982da145cf3b7a04e04
Ítalo Mutão <italo.mutao@gmail.com>
2025-10-30 22:15:27 -0300

    chore: Add arquivo de ponto de entrada principal para a aplicação

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Vertical Logistica Desafio API')
+    .setDescription(
+      'Documentação da API do desafio proposto pela Vertical Logistica',
+    )
+    .setVersion('1.0')
+    .addTag('Pedidos')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
